d8d17d4837738136650ea2d86ade27455cb0b3a6
Juan David Luna <luna.juandavid95@gmail.com>
2022-11-21 02:26:06 -0500

    Committed at 2022-11-21 02:26:00

--- app/src/main/java/com/example/taxiapp/ui/fragment/MainFragment.java
+++ app/src/main/java/com/example/taxiapp/ui/fragment/MainFragment.java
@@ -1,5 +1,8 @@
 package com.example.taxiapp.ui.fragment;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -9,12 +12,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
 
 import com.example.taxiapp.R;
+import com.google.android.material.card.MaterialCardView;
 
 public class MainFragment extends Fragment implements View.OnClickListener{
 
@@ -32,6 +38,8 @@ public class MainFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        MaterialCardView menuMaps = view.findViewById(R.id.mnu_map);
+        menuMaps.setOnClickListener(this);
     }
 
     @Override
@@ -49,8 +57,28 @@ public class MainFragment extends Fragment implements View.OnClickListener{
 
         switch (view.getId()) {
             case R.id.mnu_map:
-                NavDirections action = MainFragmentDirections.actionMainFragmentToMapsFragment();
-                Navigation.findNavController(view).navigate(action);
+                int COARSE_PERMISSION = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION);
+                int FINE_PERMISSION = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+                if (COARSE_PERMISSION== PackageManager.PERMISSION_DENIED||FINE_PERMISSION==PackageManager.PERMISSION_DENIED) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle(R.string.location_permission);
+                    builder.setMessage(R.string.explanation_location_permission);
+                    builder.setIcon(R.mipmap.globe);
+                    builder.setNegativeButton(R.string.cancel, (dialogInterface, i) -> {
+                        dialogInterface.dismiss();
+                    });
+
+                    builder.setPositiveButton(R.string.allow, (dialogInterface, i) -> {
+
+                    });
+
+                    builder.show();
+                }
+                if (Build.VERSION.SDK_INT>29) {
+                    int BACKGROUND_PERMISSION = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+                }
+                //NavDirections action = MainFragmentDirections.actionMainFragmentToMapsFragment();
+                //Navigation.findNavController(view).navigate(action);
                 break;
         }
     }
